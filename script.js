794c7add31f5e2d060824c688dc9b5aca983a535
mrdayanand <dayugani40@gmail.com>
2026-07-12 18:52:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Mr. Dayanand Official Website Loaded");
-});
+const channelId = "UCFN8vklTYO9E6r3feOJ43Ow";
+
+const videos = document.getElementById("videos");
+
+videos.innerHTML = `
+<iframe
+width="100%"
+height="315"
+src="https://www.youtube.com/embed?listType=uploads&list=${channelId}"
+title="Latest Videos"
+frameborder="0"
+allowfullscreen>
+</iframe>
+
+<br><br>
+
+<a href="https://youtube.com/@mr.dayanand" target="_blank"
+style="
+display:inline-block;
+padding:12px 24px;
+background:#ff0000;
+color:#fff;
+text-decoration:none;
+border-radius:8px;
+font-weight:bold;">
+🎵 View All Songs
+</a>
+`;
