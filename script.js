6e9df054150c878bea036cd95de5562e0faf30bd
mrdayanand <dayugani40@gmail.com>
2026-07-12 21:14:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ videos.innerHTML = `
 <iframe
 width="100%"
 height="315"
-src="https://www.youtube.com/embed?listType=uploads&list=${channelId}"
+const videos = document.getElementById("videos");
+
+videos.innerHTML = `
+<iframe
+width="100%"
+height="315"
+src="https://www.youtube.com/embed/7N5BOxLbGAw"
+frameborder="0"
+allowfullscreen>
+</iframe>
+
+<br><br>
+
+<a href="https://youtube.com/@mr.dayanand" target="_blank"
+style="display:inline-block;padding:12px 24px;background:#ff0000;color:#fff;text-decoration:none;border-radius:8px;font-weight:bold;">
+🎵 View All Songs
+</a>
+`;
 title="Latest Videos"
 frameborder="0"
 allowfullscreen>
